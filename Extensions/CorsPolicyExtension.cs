e3ef95ac4f88bfa012b03c53c0c5c70f34dba256
elvin_dzhavadov <eljav04@mail.ru>
2026-04-11 03:46:31 +0400

    fix: CORS for anyOrigin

--- Extensions/CorsPolicyExtension.cs
+++ Extensions/CorsPolicyExtension.cs
@@ -11,7 +11,8 @@ public static class CorsPolicyExtension
         policy =>
         {
             policy
-                  .AllowAnyMethod()
+			      .AllowAnyOrigin()
+				  .AllowAnyMethod()
                   .AllowAnyHeader()
                   .AllowCredentials();
         });
